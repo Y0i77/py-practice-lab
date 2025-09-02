9d257e069f7ecca2e59316bc9f335e905152eaec
Y0i77 <tu_correo_de_github@example.com>
2025-09-02 16:43:09 -0500

    New structure to order the whole content

---
+++ T1/chatActivity/FuncionDeOR.py
@@ -0,0 +1,37 @@
+﻿"""
+1. Programa que recibe dos valores (0 o 1)en dos variables x1 y x2 y devuelve la función lógica OR
+
+NOTA: para que funcione correctamente recomiendo descargar las librerias:
+                - pyinputplus
+                - rich
+    puede obtenerlas por medio del comando pip install... desde la terminal
+"""
+
+import pyinputplus as pyip
+from rich.console import Console
+console = Console()
+
+#Función para calcular los resultados
+def or_function_result(value1, value2):
+    match value1 + value2:
+        case 1: #Si la suma entre dos numeros me da 1 significa que los dos no son 0
+            console.print(1)
+        case 2: # si la suma me da 2 ambos son 1
+            console.print(1)
+        case 0: # si la suma me da 0 no tengo ningún 1
+            console.print(0)
+        case _:
+            console.print(f"[red]ERROR: {value1} or {value2} is not a supported number[/red]")
+
+x1 = pyip.inputInt(
+    prompt="Introduce a \033[31mvalue for x1:\033[0m ",
+    min=0, max=1 #Limitamos el rango para el valor de x1
+)
+
+x2 = pyip.inputInt(
+    prompt="Introduce a \033[31mvalue for x1:\033[0m ",
+    min=0, max=1 #Limitamos el rango para el valor de x2
+)
+
+or_function_result(x1,x2)
+
---
+++ T1/chatActivity/ModeloDeMcCulloch&Pitts.py
@@ -0,0 +1,59 @@
+﻿"""
+programar la neurona del Modelo McCulloch & Pitts (MCP).
+
+NOTA: para que funcione correctamente recomiendo descargar las librerias:
+                - pyinputplus (facilita el control de inserción de datos)
+                - rich (Permite mejorar la calidad de los Print clasicos)
+    puede obtenerlas por medio del comando pip install... desde la terminal
+"""
+
+#Librerias Integradas para mejorar la experiencia en la terminal
+import pyinputplus as pyip
+from rich.console import Console
+
+console = Console()
+
+THRESHOLD = 0 #lÍMITE MINIMO
+
+selectedOption = '' #Inicialización de variables para mejores prácticas
+xnValues = []
+wnValues = []
+
+
+while selectedOption != '1': #Bucle para añadir libertad de datos al usirio
+
+    selectedOption = pyip.inputStr( #Despliegue de opciones con ACSI para dar vida al menú
+        prompt="\033[34mChoose an Option\033[0m\n"
+               "\033[32m1. Start The Program\033[0m\n"
+               "\033[33m2. Add More Values\033[0m\n> "
+    )
+
+    match selectedOption: #Match para verificar la opción tomada
+        case '1' | 'start': #Opción 1 para empezar el programa con los datos agregados
+            if not xnValues or not wnValues:
+                console.print("[red]ERROR: You must add inputs and weights first.[/red]")
+                continue
+
+            total = sum(x*w for x, w in zip(xnValues, wnValues)) #Realizamos la formula
+            console.print(f"\033[35mTotal weighted sum: {total}\033[0m")
+
+            output = 1 if total >= THRESHOLD else 0 #Comprobamos el límite establecido
+            console.print(f"\033[36mNeuron output (MCP): {output}\033[0m")
+            break #volvemos a iniciar el bucle
+
+        case '2': # Caso 2 para agregar datos nuevos
+            x_val = pyip.inputInt(
+                prompt="Introduce a \033[31mvalue for x\033[0m: ")
+            xnValues.append(x_val)
+
+            w_val = pyip.inputInt(
+                prompt="Introduce a \033[31mvalue for w\033[0m: ")
+            wnValues.append(w_val)
+
+
+            THRESHOLD = pyip.inputInt(
+                prompt="Introduce a \033[33mthreshold value\033[0m (default 0): ",
+                default=0)
+
+        case _: #En caso de error
+            console.print("[red]ERROR: Not a valid option.[/red]")
